3c74c8a16a64f91c3e86eca3633487d83cd78407
Gael Ollivier <gael@ollivier.dev>
2022-07-02 19:52:18 -0700

    WIP

---
+++ src/authFlow.ts
@@ -0,0 +1,10 @@
+import { createServer } from 'http';
+
+// Serve hello world on port 3000
+createServer((req, res) => {
+  console.log(req.url);
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Hello World\n');
+}).listen(3000);
+
+console.log('Server running at http://localhost:3000/');
--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -3,4 +3,10 @@ mkdir -p /app/data/
 echo $GOOGLE_CREDENTIALS > /app/data/google-credentials.json
 echo $GOOGLE_TOKEN > /app/data/google-token.json
 
-yarn start
+# If --auth is passed, we'll use the Google Authentication flow
+if [ "$1" = "--auth" ]; then
+  echo "Using Google Authentication"
+  yarn auth-flow
+else
+  yarn start
+fi
